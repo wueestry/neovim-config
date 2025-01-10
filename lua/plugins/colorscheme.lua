a3b788a2d3e9db10546c487fca4e29a73bdfd372
Ryan Wüest <ryan.wueest@protonmail.com>
2025-01-10 11:04:04 +0100

    Apply catppuccin colourscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,59 +1,68 @@
 return {
-  "catppuccin/nvim",
-  lazy = true,
-  name = "catppuccin",
-  opts = {
-    integrations = {
-      aerial = true,
-      alpha = true,
-      cmp = true,
-      dashboard = true,
-      flash = true,
-      fzf = true,
-      grug_far = true,
-      gitsigns = true,
-      headlines = true,
-      illuminate = true,
-      indent_blankline = { enabled = true },
-      leap = true,
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        fzf = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
 
-      lsp_trouble = true,
-      mason = true,
-      markdown = true,
-      mini = true,
-      native_lsp = {
-        enabled = true,
-        underlines = {
-          errors = { "undercurl" },
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
 
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
         },
-      },
-      navic = { enabled = true, custom_bg = "lualine" },
+        navic = { enabled = true, custom_bg = "lualine" },
 
-      neotest = true,
-      neotree = true,
-      noice = true,
-      notify = true,
-      semantic_tokens = true,
-      snacks = true,
-      telescope = true,
-      treesitter = true,
-      treesitter_context = true,
-      which_key = true,
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        snacks = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        "akinsho/bufferline.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ""):find("catppuccin") then
+            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+          end
+        end,
+      },
     },
   },
-  specs = {
-    {
-      "akinsho/bufferline.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if (vim.g.colors_name or ""):find("catppuccin") then
-          opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-        end
-      end,
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
     },
   },
+  { "folke/tokyonight.nvim", enabled = false },
 }
