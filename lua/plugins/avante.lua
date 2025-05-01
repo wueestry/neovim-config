09a9e76b2e0e6fe3d7761600f201de2270ad86d1
Ryan Wüest <ryan.wueest@protonmail.com>
2025-05-01 10:02:16 +0200

    Add remote server as avante ollama instance

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,19 +3,20 @@ return {
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   opts = {
-    provider = "ollama",
+    provider = "ollama", -- Use ollama as the provider for language models
     ollama = {
-      model = "qwen2.5-coder:14b",
+      endpoint = "http://apollo:11434", -- URL of the ollama server
+      model = "qwen2.5-coder:7b", -- Specific model to use
     },
     behaviour = {
-      enable_cursor_planning_mode = true, -- recommended when using ollama
+      enable_cursor_planning_mode = true, -- Recommended when using ollama for better cursor planning
     },
   },
-  build = "make",
+  build = "make", -- Build command assumes a Makefile is present in the plugin directory
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
+    "nvim-treesitter/nvim-treesitter", -- Dependency for syntax highlighting and parsing
+    "stevearc/dressing.nvim", -- Dependency for enhanced UI components
+    "nvim-lua/plenary.nvim", -- Utility library for Neovim plugins
+    "MunifTanjim/nui.nvim", -- Library for building user interfaces in Neovim
   },
 }
