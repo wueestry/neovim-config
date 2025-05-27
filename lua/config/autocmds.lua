a80451cfc065f12511a0d024ddf9925fd36f76fa
Ryan Wüest <ryan.wueest@protonmail.com>
2025-05-27 13:01:25 +0200

    Add quickfix keymaps

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,10 +1,32 @@
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = vim.api.nvim_create_augroup("resize_splits", {clear = true}),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = vim.api.nvim_create_augroup("auto_create_dir", {clear = true}),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,3 +23,30 @@ map.set("n", "<leader>srl", "<cmd>vertical resize -3<cr>", { desc = "[S]plit [R]
 
 -- Change workingdir
 map.set("n", "<leader>wc", ":cd %:p:h<CR>:pwd<CR>", { desc = "[W]orkspace [C]hange" })
+
+-- Move Lines
+map.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+map.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+map.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+map.set("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+map.set("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+map.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+map.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
