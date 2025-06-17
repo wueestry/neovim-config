249a691824a5211e7a496be29c720687b5da87cf
Ryan Wüest <ryan.wueest@protonmail.com>
2025-06-17 09:12:51 +0200

    Add lazyvim extras

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,44 @@
+local auto_close_filetype = {
+  "lazy",
+  "mason",
+  "lspinfo",
+  "toggleterm",
+  "null-ls-info",
+  "TelescopePrompt",
+  "notify",
+}
+
+-- Auto close window when leaving
+vim.api.nvim_create_autocmd("BufLeave", {
+  group = vim.api.nvim_create_augroup("lazyvim_auto_close_win", { clear = true }),
+  callback = function(event)
+    local ft = vim.api.nvim_buf_get_option(event.buf, "filetype")
+
+    if vim.fn.index(auto_close_filetype, ft) ~= -1 then
+      local winids = vim.fn.win_findbuf(event.buf)
+      for _, win in pairs(winids) do
+        vim.api.nvim_win_close(win, true)
+      end
+    end
+  end,
+})
+
+-- Disable next line comments
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.cmd("set formatoptions-=cro")
+    vim.cmd("setlocal formatoptions-=cro")
+  end,
+})
+
+-- Create a dir when saving a file if it doesnt exist
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+  callback = function(args)
+    if args.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.uv.fs_realpath(args.match) or args.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
--- lua/extras/init.lua
+++ lua/extras/init.lua
@@ -1,5 +1,9 @@
 return {
+  { import = "lazyvim.plugins.extras.coding.blink" },
+  { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.dap.core" },
+  { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
+  { import = "lazyvim.plugins.extras.editor.snacks_picker" },
   { import = "lazyvim.plugins.extras.test.core" },
   -- Import Language Servers
   { import = "lazyvim.plugins.extras.lang.clangd" },
